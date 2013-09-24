5791b278c74dfdc953957d513b94afcbf1e0421c
闲.大赋 <xiandafu@126.com>
2013-09-24 10:13:40 +0800

    更新

---
+++ src/org/beetl/core/GroupTemplate.java
@@ -0,0 +1,29 @@
+package org.beetl.core;
+
+public class GroupTemplate {
+	
+	/** 使用loader 和 conf初始化GroupTempalte
+	 * @param loader 资源加载器
+	 * @param conf  模板引擎配置
+	 */
+	
+	public GroupTemplate(ResourceLoader loader,Configuration conf){
+		
+	}
+	/** 使用loader 和 默认的模板引擎配置来初始化GroupTempalte
+	 * @param loader 资源加载器
+	 * 
+	 */
+	
+	public GroupTemplate(ResourceLoader loader){
+		
+	}
+	
+	public Template getTemplate(String key){
+		return null;
+	}
+	
+	
+	
+	
+}

c7025163e92575ba4b6815b896ced7003d0949b6
CZJCC <dlut.zjc@gmail.com>
2014-04-18 20:10:12 +0800

    Create ArrayUtil.java

---
+++ beetl-core/src/main/java/org/beetl/ext/fn/ArrayUtil.java
@@ -0,0 +1,200 @@
+/*
+ [The "BSD license"]
+ Copyright (c) 2011-2013 Joel Li (李家智)
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions
+ are met:
+ 1. Redistributions of source code must retain the above copyright
+     notice, this list of conditions and the following disclaimer.
+ 2. Redistributions in binary form must reproduce the above copyright
+     notice, this list of conditions and the following disclaimer in the
+     documentation and/or other materials provided with the distribution.
+ 3. The name of the author may not be used to endorse or promote products
+     derived from this software without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.beetl.ext.fn;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+/**
+ * 
+ * @author 张健川  dlut.zjc@gmail.com
+ * @create 2014年4月18日 
+ */
+public class ArrayUtil
+{
+	public Object[] range(Object arrayOrCollection, int start, int end)
+	{
+		if (arrayOrCollection.getClass().isArray())
+		{
+			Object[] array = (Object[]) arrayOrCollection;
+			if (start >= end || start < 0 || end > array.length)
+				throw new RuntimeException("start,end参数设置不正确");
+			Object[] newArray = new Object[end - start];
+			for (int i = start - 1; i < end - 1; i++)
+			{
+				newArray[i - start + 1] = array[i];
+			}
+			return newArray;
+		}
+		else if (arrayOrCollection instanceof Collection)
+		{
+			Collection<Object> collections = (Collection<Object>) arrayOrCollection;
+			if (start >= end || start < 0 || end > collections.size())
+				throw new RuntimeException("start,end参数设置不正确");
+			Object[] array = new Object[end - start];
+			Iterator<Object> iterator = collections.iterator();
+			int i = 0;
+			int index = 0;
+			while (iterator.hasNext())
+			{
+				if (i >= start - 1 && i < end - 1)
+				{
+					array[index] = iterator.next();
+					index++;
+				}
+				else
+				{
+					iterator.next();
+				}
+				i++;
+			}
+			return array;
+		}
+		return null;
+	}
+
+	public Object remove(Object arrayOrCollection, Object item)
+	{
+		if (arrayOrCollection != null)
+		{
+			if (arrayOrCollection.getClass().isArray())
+			{
+				Object[] oldArray = (Object[]) arrayOrCollection;
+				Object[] newArray = new Object[oldArray.length - 1];
+				Class type = arrayOrCollection.getClass().getComponentType();
+				if (!(item.getClass().equals(type)))
+				{
+					throw new RuntimeException("item类型不正确");
+				}
+				else
+				{
+					int flag = 0;
+					for (int i = 0; i < oldArray.length; i++)
+					{
+						if (oldArray[i] == item || oldArray[i].equals(item))
+						{
+							flag = 1;
+							continue;
+						}
+						if (i - flag == oldArray.length - 1)
+							return oldArray;
+						newArray[i - flag] = oldArray[i];
+					}
+					return newArray;
+				}
+			}
+			else if (arrayOrCollection instanceof Collection)
+			{
+				((Collection) arrayOrCollection).remove(item);
+				return arrayOrCollection;
+			}
+		}
+		return null;
+	}
+
+	public Object add(Object[] params)
+	{
+		Object o = params[0];
+		Object item = params[1];
+		if (o != null)
+		{
+			if (o.getClass().isArray())
+			{
+				Object[] oldArray = (Object[]) o;
+				Object[] newArray = new Object[oldArray.length + 1];
+				int index = ((Object[]) params[0]).length;
+				if (params.length > 2)
+				{
+					index = ((Integer) params[2]).intValue();
+				}
+				Class type = o.getClass().getComponentType();
+				if (!(item.getClass().equals(type)) || index < 0 || index > oldArray.length)
+				{
+					throw new RuntimeException("item或者index参数不正确");
+				}
+				else
+				{
+					int flag = 0;
+					for (int i = 0; i < oldArray.length + 1; i++)
+					{
+						if (i != index)
+						{
+							newArray[i] = oldArray[i - flag];
+						}
+						else
+						{
+							newArray[i] = item;
+							flag = 1;
+						}
+					}
+				}
+				return newArray;
+			}
+			else if (o instanceof Collection)
+			{
+				Collection<Object> collection = (Collection<Object>) o;
+				((Collection) o).add(item);
+				return collection;
+			}
+		}
+		return null;
+	}
+
+	public Boolean contain(Object arrayOrCollection, Object item)
+	{
+		if (arrayOrCollection != null)
+		{
+			if (arrayOrCollection.getClass().isArray())
+			{
+				Object[] array = (Object[]) arrayOrCollection;
+				Class type = arrayOrCollection.getClass().getComponentType();
+				if (!(item.getClass().equals(type)))
+				{
+					throw new RuntimeException("item或者index参数不正确");
+				}
+				else
+				{
+					for (int i = 0; i < array.length; i++)
+					{
+						if (array[i].equals(item) || array[i] == item)
+						{
+							return true;
+						}
+					}
+					return false;
+				}
+			}
+			else if (arrayOrCollection instanceof Collection)
+			{
+				Collection<Object> collection = (Collection<Object>) arrayOrCollection;
+				return collection.contains(item);
+			}
+		}
+		return false;
+	}
+}

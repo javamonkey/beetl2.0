e72a96f6032226adad84d01637007a26bac6a660
wendal <wendal1985@gmail.com>
2015-09-11 03:10:49 +0800

    change: MapResourceLoader移动到org.beetl.core.resource

---
+++ beetl-core/src/main/java/org/beetl/core/resource/MapResourceLoader.java
@@ -0,0 +1,55 @@
+package org.beetl.core.resource;
+
+import java.io.Reader;
+import java.io.StringReader;
+import java.util.HashMap;
+
+import org.beetl.core.GroupTemplate;
+import org.beetl.core.Resource;
+import org.beetl.core.ResourceLoader;
+import org.beetl.core.exception.BeetlException;
+
+/**
+ * 可动态存入模板的ResourceLoader
+ * @author wendal(wendal1985@gmail.com)
+ *
+ */
+public class MapResourceLoader extends HashMap<String, String> implements ResourceLoader {
+    
+    private static final long serialVersionUID = 5361058943317379270L;
+    
+    protected boolean autoCheck;
+    
+    public boolean isModified(Resource key) {
+        return !autoCheck;
+    }
+    public void init(GroupTemplate gt) {}
+    public String getResourceId(Resource resource, String key) {
+        return key;
+    }
+    public Resource getResource(final String key) {
+        return new Resource(key, this) {
+            public Reader openReader() {
+                String val = get(key);
+                if (val == null) {
+                    BeetlException ex = new BeetlException(BeetlException.TEMPLATE_LOAD_ERROR);
+                    ex.resourceId = key;
+                    throw ex;
+                }
+                return new StringReader(val);
+            }
+            
+            public boolean isModified() {
+                return !autoCheck;
+            }
+        };
+    }
+    public boolean exist(String key) {
+        return containsKey(key);
+    }
+    public void close() {}
+    
+    public void setAutoCheck(boolean autoCheck) {
+        this.autoCheck = autoCheck;
+    }
+}
--- beetl-core/src/test/java/org/beetl/ext/nutz/BeetlViewMakerTest.java
+++ beetl-core/src/test/java/org/beetl/ext/nutz/BeetlViewMakerTest.java
@@ -6,8 +6,6 @@ import static org.mockito.Mockito.when;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.io.Reader;
-import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -18,10 +16,7 @@ import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.beetl.core.GroupTemplate;
-import org.beetl.core.Resource;
-import org.beetl.core.ResourceLoader;
-import org.beetl.core.exception.BeetlException;
+import org.beetl.core.resource.MapResourceLoader;
 import org.nutz.mvc.View;
 
 import junit.framework.TestCase;
@@ -86,38 +81,4 @@ public class BeetlViewMakerTest extends TestCase {
         when(req.getAttribute("obj")).thenReturn(re);
         return req;
     }
-}
-
-class MapResourceLoader extends HashMap<String, String> implements ResourceLoader {
-    
-    private static final long serialVersionUID = 5361058943317379270L;
-    
-    public boolean isModified(Resource key) {
-        return true;
-    }
-    public void init(GroupTemplate gt) {}
-    public String getResourceId(Resource resource, String key) {
-        return key;
-    }
-    public Resource getResource(final String key) {
-        return new Resource(key, this) {
-            public Reader openReader() {
-                String val = get(key);
-                if (val == null) {
-                    BeetlException ex = new BeetlException(BeetlException.TEMPLATE_LOAD_ERROR);
-                    ex.resourceId = key;
-                    throw ex;
-                }
-                return new StringReader(val);
-            }
-            
-            public boolean isModified() {
-                return true;
-            }
-        };
-    }
-    public boolean exist(String key) {
-        return containsKey(key);
-    }
-    public void close() {}
 }

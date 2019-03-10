47e29fa341d7df7eaa8faaeb3a60a5b7217b1d65
xiandafu <xiandafu@126.com>
2019-03-10 22:31:33 +0800

    sample

--- beetl-core/src/test/java/org/beetl/core/tag/DeleteTagTest.java
+++ beetl-core/src/test/java/org/beetl/core/tag/DeleteTagTest.java
@@ -15,6 +15,7 @@ public class DeleteTagTest extends BasicTestCase
 		User user = User.getTestUser();
 
 		Template t = gt.getTemplate("/tag/delete_template.html");
+		String old = this.getFileContent("/tag/delete_template.html");
 		this.bind(t, "user", user);
 		String str = t.render();
 		AssertJUnit.assertEquals(this.getFileContent("/tag/delete_expected.html"), str);
